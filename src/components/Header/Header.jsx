983faed90c4d58a424bba641f3e2c94d832995aa
Humayun63 <humayun126224@gmail.com>
2023-05-01 15:39:39 +0600

    header done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import logo from '../../assets/logo.png'
+import logo1 from '../../assets/logo2.png'
+import { Button, Container, Form, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
+    const location = useLocation()
+    const isTrue = location.pathname === '/' || location.pathname === '/booking';
     return (
-        <div>
-            <h2>This is header</h2>
-        </div>
+        <Navbar expand="lg" className='py-4'>
+            <Container fluid>
+                <Navbar.Brand>
+                    <Link to='/'><img src={isTrue ? logo1 : logo} style={{ width: '120px' }} /></Link>
+                </Navbar.Brand>
+                <Form className="w-25">
+                    <Form.Control
+                        type="search"
+                        placeholder="Search"
+                        aria-label="Search"
+                        className="bg-light bg-opacity-10 mx-5 text-dark fw-bold"
+                    />
+                </Form>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="ms-auto my-2 my-lg-0 d-flex align-items-center"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Link className={`text-decoration-none mx-2 ${isTrue ? 'text-light' : 'text-dark'}`} to='/booking'>News</Link>
+                        <Link className={`text-decoration-none mx-2 ${isTrue ? 'text-light' : 'text-dark'}`} to='/destination'>Destination</Link>
+                        <Link className={`text-decoration-none mx-2 ${isTrue ? 'text-light' : 'text-dark'}`} to='/blog'>Blog</Link>
+                        <Link className={`text-decoration-none mx-2 ${isTrue ? 'text-light' : 'text-dark'}`} to='/contact'>Contact</Link>
+                        <Link className={`text-decoration-none mx-2 ${isTrue ? 'text-light' : 'text-dark'}`} to='/login'>
+                            <Button variant="warning">Login</Button>
+                        </Link>
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
